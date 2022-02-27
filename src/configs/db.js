9eca30ffbc332f4d807772abf1b85c9a2804a4b8
vmk1803 <vmk1803@gmail.com>
2022-02-27 10:25:01 +0530

    changed

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const connect = () => {
-  return mongoose.connect(
-    "mongodb+srv://vmk1803:Vmuralik1@cluster0.7racz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  );
+  return mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connect;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const app = require("./index");
 const connect = require("./configs/db");
+const port = process.env.PORT || 5000;
 
-app.listen(2345, async () => {
-  console.log("Listening on port 2345...");
+app.listen(port, async () => {
+  console.log(`Listening on port ${port}`);
   await connect();
 });
